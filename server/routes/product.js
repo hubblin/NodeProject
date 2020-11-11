d6ff846f2f67c01b61d3de58794e540c070d0ac3
Haminhyuk <62636276+Haminhyuk@users.noreply.github.com>
2020-11-11 14:59:26 +0900

    add category

--- server/routes/product.js
+++ server/routes/product.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
 router.get('/painting', function(req, res) {
     res.render('product/paintingProduct');
+});
+
+router.get('/painting/pouch', function(req, res) {
+    res.render('product/paintingpouch');
+});
+
+router.get('/painting/case', function(req, res) {
+    res.render('product/paintingcase');
+});
+
+router.get('/painting/clothes', function(req, res) {
+    res.render('product/paintingclothes');
 });
 
 router.get('/making', function(req,res, next){
