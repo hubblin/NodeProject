8aa392f6f8ed387e3a520bd3da6234788744541e
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2020-11-15 14:48:03 +0900

    똥싼거 치움
    ㅋㅋㅋ

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
 
 //라우트 추가되면 여기에 넣기
 var indexRouter = require('./server/routes/index');
@@ -12,13 +13,6 @@ var reviewRouter = require('./server/routes/review');
 var orderRouter = require('./server/routes/order');
 
 var app = express();
-
-//여기가 router 설치
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/product', productRouter);
-app.use('/review', reviewRouter);
-app.use('/order', orderRouter);
 
 
 // view engine setup
@@ -30,6 +24,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(session({
+  key:'sid',
+  secret:'secret',
+  resave:false,
+  saveUninitialized:true,
+  cookie:{
+    maxAge:24000*60*60
+  }
+}));
+
+//여기가 router 설치
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/product', productRouter);
+app.use('/review', reviewRouter);
+app.use('/order', orderRouter);
 
 
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,13 +2,60 @@ const { render } = require('ejs');
 var express = require('express');
 var router = express.Router();
 
+const userController = require('../controller/userController');
+const User = new userController();
+
 /* GET users listing. */
-router.get('/sign_up', function(req, res, next) {
-  res.render('user/sign_up');
+router.get('/sign_up', User.findCategory, function(req, res, next) {
+  res.render('user/sign_up',{category:req.category});
 });
+
+// 회원가입 post 
+router.post('/sign_up',  User.signUp,function (req, res, next) {
+  if (req.check) {
+
+    res.send(`<script type="text/javascript">
+            alert("회원가입 성공"); 
+              location.href='/';
+        </script>`)
+  }
+  else{
+    res.send(`<script type="text/javascript">
+            alert("회원가입 실패"); 
+              location.href='/';
+        </script>`)
+  }
+})
 
 router.get('/sign_in', function(req, res, next){
 
   res.render('user/sign_in');
 });
+
+router.post('/sign_in', User.signIn,function(req,res,next){
+  if (req.check) {
+    res.send(`<script type="text/javascript">
+            alert("로그인 성공"); 
+              location.href='/';
+        </script>`)
+  }
+  else{
+    res.send(`<script type="text/javascript">
+            alert("로그인 실패"); 
+              location.href='/';
+        </script>`)
+  }
+})
+
+// 로그아웃
+router.get('/sign_out', (req, res, next)=>{
+  req.session.destroy();
+  res.clearCookie('sid');
+
+  res.send(`<script type="text/javascript">
+            alert("로그아웃"); 
+              location.href='/';
+        </script>`)
+})
+
 module.exports = router;
