570677e60210de48766bf6ef3e02c82d88d1c678
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2020-11-21 22:53:26 +0900

    배송지 카트 CRD까지 함
    업데이트는 나중에

---
+++ server/controller/mypageController.js
@@ -0,0 +1,108 @@
+const pool = require('../dbconfig/dbconfig');
+
+class MypageController {
+
+    //  배송지 추가 
+    async addAddress(req, res, next) {
+        pool.getConnection((err, conn) => {
+            if (err) throw err;
+
+            conn.query('insert into address values(?,?,?,?,?)', [
+                null, req.body.post_num, req.body.main_adr, req.body.detail_adr, req.session.user.id
+            ], (err) => {
+                if (err) throw err;
+
+                conn.release();
+                next();
+            })
+        })
+    }
+
+    // 카드 추가
+    async addCard(req, res, next){
+        pool.getConnection((err, conn)=>{
+            if(err) throw err;
+
+            conn.query('insert into card values(?,?,?,?)',[
+                req.body.card_num, req.body.card_CVC, req.body.card_date, req.session.user.id
+            ], (err)=>{
+                if(err) throw err;
+
+                conn.release();
+                next();
+
+            })
+
+        })
+    }
+
+    // 배송지 삭제
+    async deleteAddress(req, res, next){
+        pool.getConnection((err, conn)=>{
+            if(err) throw err;
+
+            conn.query('delete from address where adr_uid = ?',[
+                req.params.adr_num
+            ], (err)=>{
+                if(err) throw err;
+
+                conn.release();
+                next();
+            })
+        })
+    }
+
+    // 카드 삭제
+    async deleteCard(req, res, next){
+        pool.getConnection((err, conn)=>{
+            if(err) throw err;
+
+            conn.query('delete from card where card_num = ?',[
+                req.params.card_num
+            ], (err)=>{
+                if(err) throw err;
+
+                conn.release();
+                next();
+            })
+        })
+    }
+
+    // 사용자의 배송지 목록 가져오기
+    async getAddress(req, res, next){
+        pool.getConnection((err, conn)=>{
+            if(err) throw err;
+
+            conn.query('select * from address where adr_user_id = ?',[
+                req.session.user.id
+            ], (err, user_address)=>{
+                if(err) throw err;
+
+                req.address = user_address;
+                conn.release();
+                next();
+
+            })
+        })
+    }
+
+    // 사용자의 카드 목록 가져오기
+    async getCard(req, res, next){
+        pool.getConnection((err, conn)=>{
+            if(err) throw err;
+
+            conn.query('select * from card where card_user_id = ?',[
+                req.session.user.id
+            ], (err, user_card)=>{
+                if(err) throw err;
+
+                req.card = user_card;
+                conn.release();
+                next();
+            })
+        })
+    }
+
+}
+
+module.exports = MypageController;
