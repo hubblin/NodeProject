63c112eb83009c7a25b9831b810091555f3e7e69
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2020-11-24 22:54:09 +0900

    업체 회원가입 만듬
    아직 DB 연결 안함

--- server/routes/company.js
+++ server/routes/company.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 
 router.get('/', function(req, res, next){
-    res.render('company_page/company_mypage')
+    res.render('company_page/company_mypage',{name:req.session.user.id})
 })
 
 module.exports = router;
